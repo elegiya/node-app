705fc528cef12b20d8476999099b76a7b636d880
Maryna Gaponova <globallogic_marynag@nice.com>
2022-05-21 13:36:28 +0300

    Add express.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('In the middleware');
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log('In another middleware');
+});
+
+const server = http.createServer();
 
 server.listen(3000);
